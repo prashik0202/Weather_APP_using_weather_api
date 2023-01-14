c98bf01ede534b985ea0d94aaa1c9ea935e64120
Prashik <gamreprashik03@gmail.com>
2023-01-14 23:04:03 +0530

    api fetched and parse the data

--- server.js
+++ server.js
@@ -15,8 +15,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','ejs');
 
 app.get('/',(req,res) => {
-    res.render('index');
+    res.render('index',{ weather : null , error : null });
 })
+
+//for handling the wronge address:
+app.get('*',(req,res) => {
+    return res.status(404).render('404');
+})
+
+app.post('/',(req,res) => {
+    const city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`;
+
+    request(url,(err,response,body) => {
+        if(err){
+            res.render('index',{ weather : null , error : 'Error, please try again'});
+        }else{
+            let weather = JSON.parse(body);
+            if(weather.main == undefined){
+                res.render('index', {weather : null , error : 'Error, please try again' });
+            }else{
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}`;
+                res.render('index',{ weather : weatherText , error : null });
+            }
+        }
+    });
+});
 
 app.listen(port,() => {
     console.log(`Server is started on : http://localhost:${port}`);
